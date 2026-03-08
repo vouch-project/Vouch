1ee2625ee265c6dc76d8ad4b90f3e3756d08bcdd
Nadav Barak <nadavb227@gmail.com>
2026-03-08 21:09:01 +0200

    fix: add eslint config for apps/web

---
+++ apps/web/eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ['build/', '.svelte-kit/', 'dist/'],
+  },
+];
