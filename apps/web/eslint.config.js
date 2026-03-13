963b577a1843103c430b201bb3ad2b9d2c02c46c
Nir Arad <nirarad86@gmail.com>
2026-03-13 04:08:26 +0200

    fix: eslint config

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -3,6 +3,11 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import svelteParser from 'svelte-eslint-parser';
 import tseslint from 'typescript-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default tseslint.config(
   js.configs.recommended,
