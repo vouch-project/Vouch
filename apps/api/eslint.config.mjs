bd8e73cf9524c1044a7744721bd42f03f0a3b47f
NirArad10 <128100633+NirArad10@users.noreply.github.com>
2026-03-13 22:16:28 +0200

    feat: api eslint (#37)

--- apps/api/eslint.config.mjs
+++ apps/api/eslint.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -29,7 +29,9 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'func-style': ['error', 'expression'],
+      'prefer-arrow-callback': 'error',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
