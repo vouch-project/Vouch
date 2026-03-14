5aa6f68d7ce94701567e7f46544d742551536fb6
Nir Arad <nirarad86@gmail.com>
2026-03-14 17:14:21 +0200

    oopsy

--- apps/web/src/lib/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export const objectFromEntries = <T extends ReadonlyArray<readonly [PropertyKey, unknown]>>(
-  entries: T,
-): { [K in T[number] as K[0]]: K[1] } => Object.fromEntries(entries) as never;
