cc6176da64f7c05ead1ae817b892041fdefcb533
Nir Arad <nirarad86@gmail.com>
2026-03-14 05:10:00 +0200

    fix: eslint

--- packages/config/eslint.config.mjs
+++ packages/config/eslint.config.mjs
@@ -1,6 +1,6 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
@@ -24,6 +24,7 @@ export default [
       '**/.svelte-kit/**',
       '**/artifacts/**',
       '**/typechain-types/**',
+      'eslint.config.mjs',
     ],
   },
 ];
