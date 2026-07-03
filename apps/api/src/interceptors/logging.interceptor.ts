f8090092f09c3b9cc0fe5dfc1bdb89b219ff8f29
Nir Arad <nirarad86@gmail.com>
2026-07-03 18:07:36 +0300

    fix

--- apps/api/src/interceptors/logging.interceptor.ts
+++ apps/api/src/interceptors/logging.interceptor.ts
@@ -1,14 +1,12 @@
 import {
   CallHandler,
   ExecutionContext,
-  Injectable,
   Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import type { Request, Response } from 'express';
 import { Observable, tap } from 'rxjs';
 
-@Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   private readonly logger = new Logger('HTTP');
 
